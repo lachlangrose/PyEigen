dd5a817c39b1f0561d83c03db536a6e9a301e9aa
Lachlan Grose <lachlan.grose@monash.edu>
2019-01-25 11:41:38 +1100

    Added submodule for pybind11

--- eigen_wrapper.cpp
+++ eigen_wrapper.cpp
@@ -1,1 +1,3 @@
-#include <pybind11>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>       //auto conversion b/w stl and python types
+#include <pybind11/iostream.h>  //auto conversion b/w stl and python types
